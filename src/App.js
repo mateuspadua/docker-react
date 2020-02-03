d1bbf1e67008917694ac5d523ba4efe170abf096
Mateus Padua <mateuspaduaweb@gmail.com>
2020-02-02 21:45:05 -0300

    chage app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ei pin yaaa 2223.
+          Ei pin yaaa 2223 aaaaaaa.
         </p>
         <a
           className="App-link"
